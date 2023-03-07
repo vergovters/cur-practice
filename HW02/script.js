d54aff3e7da20d2897c247ffb5070c394509a81c
Illia Fialkivskiji <fialkivskiji@gmail.com>
2023-03-07 16:48:24 +0200

    refactored

--- HW02/script.js
+++ HW02/script.js
@@ -1,65 +1,33 @@
 
-let numberN = 0;
-let numberM = 0;
+let numberN;
+let numberM;
 let sum = 0;
 
-while (true) {
+while (!(Number.isInteger(numberN)) || numberN < 0) {
     numberN = +prompt("Enter the number N");
-    if(!(Number.isInteger(numberN))) {
-        alert("N donsn't fit")
-    }else{
-        break
-    }
 }
 
 
 
-while (true) {
+while (!(Number.isInteger(numberM)) || numberN >= numberM) {
     numberM = +prompt("Enter the number M");
-    if(!(Number.isInteger(numberM)) ) {
-        alert("M donsn't fit")
-    }else if(numberM<numberN){
-        alert("M donsn't fit")
-    } else{
-        break
-    }
 }
 
 
-let includePairedNum;
-
-while (true) {
-    includePairedNum = prompt("Include paired numbers?(yes/no)");
-    if(includePairedNum === "yes") {
-        break
-    }
-    else if(includePairedNum === "no" ){
-        break
-    }
-    else{
-        console.log(includePairedNum);
-        alert("Answer doesn't fit")
-    }
-}
+const includePairedNum = confirm("Add paired numbers?")
 
 
-switch(includePairedNum) {
-    case "yes":
-        for( i = numberN; i <= numberM; i++) {
-            sum = sum + i;
-          }
-        break
-    case "no":
-        console.log("no")
-        for( i = numberN; i <= numberM; i++) {
-            if(i % 2 == 0) continue
-            sum = sum + i;
-          }
-        break
+for( i = numberN; i <= numberM; i++) {
+    if(includePairedNum == true){
+        sum = sum + i;
+    }else {
+        if(i % 2 == 0) continue
+        sum = sum + i;
+    }
+    
 }
 
 const result = "Number N = " + numberN + "\nNumber M = " + numberM + "\nInclude paired numbers? " + includePairedNum + "\nResult = " + sum;
 alert(result)
-
 
 
